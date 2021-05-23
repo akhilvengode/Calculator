2592c756c1dce32ab38a94b7a07d604ef490030a
Akhil Lekha <akhillekha@qburst.com>
2021-05-24 00:53:05 +0530

    responsive completed

--- webpack/webpack.common.ts
+++ webpack/webpack.common.ts
@@ -28,7 +28,7 @@ const config: webpack.Configuration = {
     output: {
         path: path.join(__dirname, "../build"),
         filename: "index_bundle.js",
-        publicPath: "/Calculator"
+        publicPath: "/"
     },
     module: {
         rules: [
